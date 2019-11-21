36f3c10d6bcfc0e3b945c4bc4ea70caff807ea9f
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-11-21 23:25:33 +0530

    correction

--- lotus/packages/perforce.scm
+++ lotus/packages/perforce.scm
@@ -19,7 +19,7 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (lotus packages perforce)
-  #:use-module ((guix licenses) #:hide (zlib))
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
