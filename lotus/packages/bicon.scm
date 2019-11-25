c838501d0314bf24aee0a215e27bb45a76a00727
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-11-25 22:43:51 +0530

    correction

--- lotus/packages/bicon.scm
+++ lotus/packages/bicon.scm
@@ -18,7 +18,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (lotus packages fribidi)
+(define-module (lotus packages bicon)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
