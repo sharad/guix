60f55a623994f7c121a81119a5edd55d07af88e6
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-12-02 23:19:39 +0530

    correction

---
+++ lotus/packages/pam.scm
@@ -0,0 +1,3 @@
+
+;; from https://cdn-aws.deb.debian.org/debian/pool/main/p/pam-tmpdir/pam-tmpdir_0.09.tar.gz
+;; 'https://cdn-aws.deb.debian.org/debian/pool/main/p/pam-tmpdir/pam-tmpdir_0.09.tar.gz'
