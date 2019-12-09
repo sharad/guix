60b7abcda2bfd136a3f6cbda423319c2f8ebf10a
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-12-09 21:28:34 +0530

    correction

--- lotus/packages/python-xyz.scm
+++ lotus/packages/python-xyz.scm
@@ -1,77 +1,80 @@
+
+
+
 (define-module (lotus packages python-xyz)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
   #:use-module (gnu packages python-xyz))
 
 
-(define-public python-colorama-043
-  (package (inherit python-colorama)
-           (version "0.4.3")
-           (source
-            (origin
-              (method url-fetch)
-              (uri (pypi-uri "colorama" version))
-              (sha256
-               (base32
-                "189n8hpijy14jfan4ha9f5n06mnl33cxz7ay92wjqgkr639s0vg9"))))))
+;; (define-public python-colorama-043
+;;   (package (inherit python-colorama)
+;;            (version "0.4.3")
+;;            (source
+;;             (origin
+;;               (method url-fetch)
+;;               (uri (pypi-uri "colorama" version))
+;;               (sha256
+;;                (base32
+;;                 "189n8hpijy14jfan4ha9f5n06mnl33cxz7ay92wjqgkr639s0vg9"))))))
 
-(define-public python-pyyaml-52
-  (package (inherit python-pyyaml)
-           (name "python-pyyaml")
-           (version "5.2")
-           (source
-            (origin
-              (method url-fetch)
-              (uri (pypi-uri "PyYAML" version))
-              (sha256
-               (base32
-                "0v1lwxbn0x2s6lbj7iqbn4gpy6rxf17jqvpcqb1jjbaq5k58xvn0"))))))
+;; (define-public python-pyyaml-52
+;;   (package (inherit python-pyyaml)
+;;            (name "python-pyyaml")
+;;            (version "5.2")
+;;            (source
+;;             (origin
+;;               (method url-fetch)
+;;               (uri (pypi-uri "PyYAML" version))
+;;               (sha256
+;;                (base32
+;;                 "0v1lwxbn0x2s6lbj7iqbn4gpy6rxf17jqvpcqb1jjbaq5k58xvn0"))))))
 
-(define-public python-exifread
-  (package
-    (name "python-exifread")
-    (version "2.1.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "ExifRead" version))
-       (sha256
-        (base32
-         "1b90jf6m9vxh9nanhpyvqdq7hmfx5iggw1l8kq10jrs6xgr49qkr"))))
-    (build-system python-build-system)
-    (home-page "https://github.com/ianare/exif-py")
-    (synopsis
-     "Read Exif metadata from tiff and jpeg files.")
-    (description
-     "Read Exif metadata from tiff and jpeg files.")
-    (license license:bsd-3)))
+;; (define-public python-exifread
+;;   (package
+;;     (name "python-exifread")
+;;     (version "2.1.2")
+;;     (source
+;;      (origin
+;;        (method url-fetch)
+;;        (uri (pypi-uri "ExifRead" version))
+;;        (sha256
+;;         (base32
+;;          "1b90jf6m9vxh9nanhpyvqdq7hmfx5iggw1l8kq10jrs6xgr49qkr"))))
+;;     (build-system python-build-system)
+;;     (home-page "https://github.com/ianare/exif-py")
+;;     (synopsis
+;;      "Read Exif metadata from tiff and jpeg files.")
+;;     (description
+;;      "Read Exif metadata from tiff and jpeg files.")
+;;     (license license:bsd-3)))
 
-(define-public python-organize-tool
-  (package
-    (name "python-organize-tool")
-    (version "1.7.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "organize-tool" version))
-       (sha256
-        (base32
-         "15yyh3ycb1f7q5ig7gq5ppwaf1wpx5dr906w80rfzlkh0z8byvh3"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (delete 'check))))
-    (inputs
-     `(("python-exifread"     ,python-exifread)
-       ("python-docopt"       ,python-docopt)
-       ("python-appdirs"      ,python-appdirs)
-       ("python-send2trash"   ,python-send2trash)
-       ("python-pyyaml"       ,python-pyyaml-52)
-       ("python-colorama"     ,python-colorama-043)))
-    (home-page
-     "https://github.com/tfeldmann/organize")
-    (synopsis "The file management automation tool")
-    (description
-     "The file management automation tool")
-    (license license:expat)))
+;; (define-public python-organize-tool
+;;   (package
+;;     (name "python-organize-tool")
+;;     (version "1.7.0")
+;;     (source
+;;      (origin
+;;        (method url-fetch)
+;;        (uri (pypi-uri "organize-tool" version))
+;;        (sha256
+;;         (base32
+;;          "15yyh3ycb1f7q5ig7gq5ppwaf1wpx5dr906w80rfzlkh0z8byvh3"))))
+;;     (build-system python-build-system)
+;;     (arguments
+;;      `(#:phases
+;;        (modify-phases %standard-phases
+;;          (delete 'check))))
+;;     (inputs
+;;      `(("python-exifread"     ,python-exifread)
+;;        ("python-docopt"       ,python-docopt)
+;;        ("python-appdirs"      ,python-appdirs)
+;;        ("python-send2trash"   ,python-send2trash)
+;;        ("python-pyyaml"       ,python-pyyaml-52)
+;;        ("python-colorama"     ,python-colorama-043)))
+;;     (home-page
+;;      "https://github.com/tfeldmann/organize")
+;;     (synopsis "The file management automation tool")
+;;     (description
+;;      "The file management automation tool")
+;;     (license license:expat)))
--- lotus/packages/utils.scm
+++ lotus/packages/utils.scm
@@ -16,81 +16,81 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-;; (define-module (lotus packages utils)
-;;   #:use-module (guix packages)
-;;   #:use-module (guix download)
-;;   #:use-module (guix git-download)
-;;   #:use-module (guix build-system gnu)
-;;   #:use-module ((guix licenses) #:prefix license:)
-;;   #:use-module (gnu packages autotools)
-;;   #:use-module (gnu packages base)
-;;   #:use-module (gnu packages compression)
-;;   #:use-module (gnu packages pkg-config)
-;;   #:use-module (gnu packages dbm)
-;;   #:use-module (gnu packages onc-rpc)
-;;   #:use-module (gnu packages perl))
+(define-module (lotus packages utils)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages dbm)
+  #:use-module (gnu packages onc-rpc)
+  #:use-module (gnu packages perl))
 
-;; ;; https://issues.guix.gnu.org/issue/35619
+;; https://issues.guix.gnu.org/issue/35619
 
-;; (define-public lesspipe
-;;   (package
-;;     (name "lesspipe")
-;;     (version "1.84")
-;;     (source (origin
-;;               (method git-fetch)
-;;               (uri (git-reference
-;;                     (url "https://github.com/wofr06/lesspipe.git")
-;;                     (commit version)))
-;;               (sha256
-;;                (base32
-;;                 "124ffhzrikr88ab14rk6753n8adxijpmg7q3zx7nmqc52wpkfd8q"))))
-;;     (build-system gnu-build-system)
-;;     ;; (inputs `(("bdb" ,bdb)
-;;     ;;           ("libnsl" ,libnsl)))
-;;     (inputs
-;;      `(("bzip2"    ,bzip2)
-;;        ("perl" ,perl)))
-;;     (arguments
-;;      '(#:tests? #f
-;;        #:make-flags (let ((out  (assoc-ref %outputs "out")))
-;;                       (list
-;;                        (string-append "PREFIX=" out)))
-;;        #:phases
-;;        (modify-phases %standard-phases
-;;          (delete 'configure))))
-;;     (synopsis "lesspipe.sh, a preprocessor for less")
-;;     (description "To browse files under UNIX the excellent viewer less [1] can be used. By
-;;  setting the environment variable LESSOPEN, less can be enhanced by external
-;;  filters to become even more powerful. Most Linux distributions come already
-;;  with a \"lesspipe.sh\" that covers the most common situations.
+(define-public lesspipe
+  (package
+    (name "lesspipe")
+    (version "1.84")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/wofr06/lesspipe.git")
+                    (commit version)))
+              (sha256
+               (base32
+                "124ffhzrikr88ab14rk6753n8adxijpmg7q3zx7nmqc52wpkfd8q"))))
+    (build-system gnu-build-system)
+    ;; (inputs `(("bdb" ,bdb)
+    ;;           ("libnsl" ,libnsl)))
+    (inputs
+     `(("bzip2"    ,bzip2)
+       ("perl" ,perl)))
+    (arguments
+     '(#:tests? #f
+       #:make-flags (let ((out  (assoc-ref %outputs "out")))
+                      (list
+                       (string-append "PREFIX=" out)))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure))))
+    (synopsis "lesspipe.sh, a preprocessor for less")
+    (description "To browse files under UNIX the excellent viewer less [1] can be used. By
+ setting the environment variable LESSOPEN, less can be enhanced by external
+ filters to become even more powerful. Most Linux distributions come already
+ with a \"lesspipe.sh\" that covers the most common situations.
 
-;;  The input filter for less described here is called \"lesspipe.sh\". It is able
-;;  to process a wide variety of file formats. It enables users to deeply inspect
-;;  archives and to display the contents of files in archives without having to
-;;  unpack them before. That means file contents can be properly interpreted even
-;;  if the files are compressed and contained in a hierarchy of archives (often
-;;  found in RPM or DEB archives containing source tarballs). The filter is easily
-;;  extensible for new formats.
+ The input filter for less described here is called \"lesspipe.sh\". It is able
+ to process a wide variety of file formats. It enables users to deeply inspect
+ archives and to display the contents of files in archives without having to
+ unpack them before. That means file contents can be properly interpreted even
+ if the files are compressed and contained in a hierarchy of archives (often
+ found in RPM or DEB archives containing source tarballs). The filter is easily
+ extensible for new formats.
 
-;;  The input filter which is also called \"lesspipe.sh\" is written in a ksh
-;;  compatible language (ksh, bash, zsh) as one of these is nearly always installed
-;;  on UNIX systems and uses comparably few resources. Otherwise an implementation
-;;  in perl for example would have been somewhat simpler to code. The code looks
-;;  less clean than it could as it was tried to make the script compatible with
-;;  a number of old shells and applications especially found on non Linux systems.
+ The input filter which is also called \"lesspipe.sh\" is written in a ksh
+ compatible language (ksh, bash, zsh) as one of these is nearly always installed
+ on UNIX systems and uses comparably few resources. Otherwise an implementation
+ in perl for example would have been somewhat simpler to code. The code looks
+ less clean than it could as it was tried to make the script compatible with
+ a number of old shells and applications especially found on non Linux systems.
 
-;;  The filter does different things depending on the file format. In most cases
-;;  it is determined on the output of the \"file\" command [2], [6], that recognizes
-;;  lots of formats. Only in a few cases the file suffix is used to determine what
-;;  to display. Up to date file descriptions are included in the \"file\" package.
-;;  Maintaining a list of file formats is therefore only a matter of keeping that
-;;  package up to date.")
-;;     (home-page "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html")
-;;     (license license:ibmpl1.0)))
+ The filter does different things depending on the file format. In most cases
+ it is determined on the output of the \"file\" command [2], [6], that recognizes
+ lots of formats. Only in a few cases the file suffix is used to determine what
+ to display. Up to date file descriptions are included in the \"file\" package.
+ Maintaining a list of file formats is therefore only a matter of keeping that
+ package up to date.")
+    (home-page "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html")
+    (license license:ibmpl1.0)))
 
 
-;; ;; (define-public pi-hole
-;; ;;   "https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh")
+;; (define-public pi-hole
+;;   "https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh")
 
 
 
