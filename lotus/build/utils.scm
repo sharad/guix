776a5145b2be66c35b2bff7f2968eb9ed12510c3
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-12-24 18:51:28 +0530

    correction

--- lotus/build/utils.scm
+++ lotus/build/utils.scm
@@ -1,101 +1,44 @@
-;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2019 Brant Gardner <bcg@member.fsf.org>
-;;;
-;;; This file is part of GNU Guix.
-;;;
-;;; GNU Guix is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
-;;;
-;;; GNU Guix is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (lotus build utils)
-  #:use-module (guix packages)
-  #:use-module (guix download)
-  #:use-module (guix git-download)
-  #:use-module (guix build-system gnu)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (gnu packages autotools)
-  #:use-module (gnu packages base)
-  #:use-module (gnu packages compression)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages dbm)
-  #:use-module (gnu packages onc-rpc)
-  #:use-module (gnu packages perl))
+  #:use-module (ice-9 ftw)
+  #:use-module (srfi srfi-26)
+  ;; #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build utils)
+  ;; #:use-module (guix packages)
+  ;; #:use-module (guix download)
+  ;; #:use-module (guix build-system gnu)
+  ;; #:use-module (guix build-system trivial)
+  ;; #:use-module (gnu packages)
+  ;; #:use-module (gnu packages bootstrap)
+  ;; #:use-module (gnu packages base)
+  ;; #:use-module (gnu packages gcc)
+  ;; #:use-module (gnu packages compression)
+  ;; #:use-module (gnu packages elf)
+  ;; #:use-module (gnu packages xorg)
+  ;; #:use-module (gnu packages gtk)
+  #:export (library-file?
+            elf-binary-file?
+            regular-file?
+            directory?
+            directory-list-files))
 
-;; https://issues.guix.gnu.org/issue/35619
+(define (library-file? file)
+  (and (eq? 'regular (stat:type (stat file)))
+       (string-suffix? ".so" file)))
 
-(define-public lesspipe
-  (package
-    (name "lesspipe")
-    (version "lesspipe")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/wofr06/lesspipe.git")
-                    (commit version)))
-              (sha256
-               (base32
-                "1pw9mwwfx0k7xqriavhiffh706sh44gfl991c4afh84903wabq7l"))))
-    (build-system gnu-build-system)
-    ;; (inputs `(("bdb" ,bdb)
-    ;;           ("libnsl" ,libnsl)))
-    (inputs
-     `(("bzip2" ,bzip2)
-       ("perl"  ,perl)))
-    (arguments
-     '(#:tests? #f
-       #:make-flags (let ((out  (assoc-ref %outputs "out")))
-                      (list
-                       (string-append "PREFIX=" out)))
-       #:phases
-       (modify-phases %standard-phases
-         (delete 'configure))))
-    (synopsis "lesspipe.sh, a preprocessor for less")
-    (description "To browse files under UNIX the excellent viewer less [1] can be used. By
- setting the environment variable LESSOPEN, less can be enhanced by external
- filters to become even more powerful. Most Linux distributions come already
- with a \"lesspipe.sh\" that covers the most common situations.
+(define (elf-binary-file? file)
+  (and (eq? 'regular (stat:type (stat file)))
+       (not (string-suffix? ".so" file))
+       (executable-file? file)
+       (elf-file? file)))
 
- The input filter for less described here is called \"lesspipe.sh\". It is able
- to process a wide variety of file formats. It enables users to deeply inspect
- archives and to display the contents of files in archives without having to
- unpack them before. That means file contents can be properly interpreted even
- if the files are compressed and contained in a hierarchy of archives (often
- found in RPM or DEB archives containing source tarballs). The filter is easily
- extensible for new formats.
+(define (regular-file? file)
+  (and (not (library-file? file))
+       (not (elf-binary-file? file))))
 
- The input filter which is also called \"lesspipe.sh\" is written in a ksh
- compatible language (ksh, bash, zsh) as one of these is nearly always installed
- on UNIX systems and uses comparably few resources. Otherwise an implementation
- in perl for example would have been somewhat simpler to code. The code looks
- less clean than it could as it was tried to make the script compatible with
- a number of old shells and applications especially found on non Linux systems.
+(define (directory? file)
+  (let ((stat (stat file)))
+    (eq? 'directory (stat:type stat))))
 
- The filter does different things depending on the file format. In most cases
- it is determined on the output of the \"file\" command [2], [6], that recognizes
- lots of formats. Only in a few cases the file suffix is used to determine what
- to display. Up to date file descriptions are included in the \"file\" package.
- Maintaining a list of file formats is therefore only a matter of keeping that
- package up to date.")
-    (home-page "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html")
-    (license license:ibmpl1.0)))
-
-
-(define-public pi-hole
-  "https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh")
-
-
-
-
-
-
-
-
+(define (directory-list-files dir)
+  (scandir dir (negate (cut member <> '("." "..")))))
