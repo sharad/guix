0706adf193954960c5740ef9aa9e11a27fa615d2
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-12-31 18:14:44 +0530

    correction

--- lotus/build/patchelf-utils.scm
+++ lotus/build/patchelf-utils.scm
@@ -1,9 +1,9 @@
 
 (define-module (lotus build patchelf-utils)
   #:use-module (ice-9 ftw)
-  #:use-module (srfi srfi-26)
+  ;; #:use-module (srfi srfi-26)
   ;; #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix utils)
+  ;; #:use-module (guix utils)
   #:use-module (guix build utils)
   #:use-module (gnu packages bootstrap)
   ;; #:use-module (guix packages)
