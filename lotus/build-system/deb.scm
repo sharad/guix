9a11ec95f816f5586e20b221b694e362024dac0e
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-01-01 01:37:20 +0530

    correction

--- lotus/build-system/deb.scm
+++ lotus/build-system/deb.scm
@@ -16,9 +16,9 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (lotus build-system rpm)
-  #:use-module ((lotus build rpm-build-system))
-  ;; #:use-module ((lotus build rpm-utils))
+(define-module (lotus build-system deb)
+  #:use-module ((lotus build deb-build-system))
+  ;; #:use-module ((lotus build deb-utils))
   ;; #:select (%default-include %default-exclude)
   #:use-module (guix store)
   #:use-module (guix utils)
@@ -30,38 +30,38 @@
   #:use-module ((lotus build-system patchelf) #:prefix patchelf:)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-26)
-  #:export (%rpm-build-system-modules
-            rpm-build
-            rpm-build-system))
+  #:export (%deb-build-system-modules
+            deb-build
+            deb-build-system))
 ;; #:re-export (%default-include         ;for convenience
 ;;              %default-exclude)
 
 ;; Commentary:
 ;;
-;; Standard build procedure for Rpm packages.  This is implemented as an
+;; Standard build procedure for Deb packages.  This is implemented as an
 ;; extension of 'gnu-build-system'.
 ;;
 ;; Code:
 
-(define %rpm-build-system-modules
+(define %deb-build-system-modules
   ;; Build-side modules imported by default.
-  `((lotus build rpm-build-system)
+  `((lotus build deb-build-system)
     ,@patchelf:%patchelf-build-system-modules))
 
-(define (default-rpm)
-  "Return the default Rpm package."
+(define (default-deb)
+  "Return the default Deb package."
   ;; Lazily resolve the binding to avoid a circular dependency.
-  (let ((rpm-mod (resolve-interface '(gnu packages elf))))
-    (module-ref rpm-mod 'rpm)))
+  (let ((deb-mod (resolve-interface '(gnu packages elf))))
+    (module-ref deb-mod 'deb)))
 
 (define* (lower name
                 #:key source inputs native-inputs outputs system target ;; host-inputs
-                (rpm (default-rpm))
+                (deb (default-deb))
                 #:allow-other-keys
                 #:rest arguments)
   "Return a bag for NAME."
   (define private-keywords
-    '(#:target #:rpm #:inputs #:native-inputs)) ;#:host-inputs
+    '(#:target #:deb #:inputs #:native-inputs)) ;#:host-inputs
 
   (and (not target)                               ;XXX: no cross-compilation
        (bag
@@ -73,7 +73,7 @@
                         ,@inputs
                         ;; Keep the standard inputs of 'gnu-build-system'.
                         ,@(standard-packages)))
-         (build-inputs `(("rpm" ,rpm)
+         (build-inputs `(("deb" ,deb)
                          ,@native-inputs))
          ;; (build-inputs `(,@(if source
          ;;                       `(("source" ,source))
@@ -86,29 +86,29 @@
          ;;                       (standard-packages)
          ;;                       '())))
          (outputs outputs)
-         (build rpm-build)
+         (build deb-build)
          (arguments (strip-keyword-arguments private-keywords arguments)))))
 
-(define* (rpm-build store name inputs
+(define* (deb-build store name inputs
                          #:key source ;; host-inputs
                          (tests? #f)
                          (parallel-tests? #t)
                          (test-command ''("make" "check"))
-                         (phases '(@ (lotus build rpm-build-system)
+                         (phases '(@ (lotus build deb-build-system)
                                      %standard-phases))
                          (outputs '("out"))
                          (output-libs ''("/lib"))
                          (search-paths '())
                          (system (%current-system))
                          (guile #f)
-                         (imported-modules %rpm-build-system-modules)
-                         (modules '((lotus build rpm-build-system)
+                         (imported-modules %deb-build-system-modules)
+                         (modules '((lotus build deb-build-system)
                                     (guix build utils))))
-  "Build SOURCE using RPM, and with INPUTS."
+  "Build SOURCE using DEB, and with INPUTS."
   (define builder
     `(begin
        (use-modules ,@modules)
-       (rpm-build #:name ,name
+       (deb-build #:name ,name
                        #:source ,(match (assoc-ref inputs "source")
                                    (((? derivation? source))
                                     (derivation->output-path source))
@@ -144,10 +144,10 @@
                                 #:outputs outputs
                                 #:guile-for-build guile-for-build))
 
-(define rpm-build-system
+(define deb-build-system
   (build-system
-    (name 'rpm)
-    (description "The build system for Rpm packages")
+    (name 'deb)
+    (description "The build system for Deb packages")
     (lower lower)))
 
-;;; rpm.scm ends here
+;;; deb.scm ends here
