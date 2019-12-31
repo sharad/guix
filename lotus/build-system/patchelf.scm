3b0f29e8e73cd3d248af5ab06f74b67b4060252e
Sharad Pratap <110339+sharad@users.noreply.github.com>
2019-12-31 21:15:45 +0530

    correction

--- lotus/build-system/patchelf.scm
+++ lotus/build-system/patchelf.scm
@@ -45,7 +45,7 @@
 (define %patchelf-build-system-modules
   ;; Build-side modules imported by default.
   `((lotus build patchelf-build-system)
-    ;; (lotus build patchelf-utils)
+    (lotus build patchelf-utils)
     ,@%gnu-build-system-modules))
 
 (define (default-patchelf)
@@ -102,9 +102,9 @@
                          (system (%current-system))
                          (guile #f)
                          (imported-modules %patchelf-build-system-modules)
-                         (modules '(;; (lotus build patchelf-utils)
-                                    (lotus build patchelf-build-system)
-                                    (guix build utils))))
+                         (modules '((lotus build patchelf-build-system)
+                                    (guix build utils)
+                                    (lotus build patchelf-utils))))
   "Build SOURCE using PATCHELF, and with INPUTS."
   (define builder
     `(begin
