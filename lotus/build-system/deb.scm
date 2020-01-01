42567fd0f41742b776b8cd65314c202e7cf7a058
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-01-01 17:31:20 +0530

    correction

---
+++ lotus/build-system/deb.scm
@@ -0,0 +1,155 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 Federico Beffa <beffa@fbengineering.ch>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (lotus build-system deb)
+  #:use-module ((lotus build deb-build-system))
+  ;; #:use-module ((lotus build deb-utils))
+  ;; #:select (%default-include %default-exclude)
+  #:use-module (guix store)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (guix derivations)
+  #:use-module (guix search-paths)
+  #:use-module (guix build-system)
+  #:use-module ((guix build-system gnu) #:prefix gnu:)
+  #:use-module ((lotus build-system patchelf) #:prefix patchelf:)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-26)
+  #:use-module (gnu packages base)
+  #:export (%deb-build-system-modules
+            deb-build
+            deb-build-system))
+;; #:re-export (%default-include         ;for convenience
+;;              %default-exclude)
+
+;; Commentary:
+;;
+;; Standard build procedure for Deb packages.  This is implemented as an
+;; extension of 'gnu-build-system'.
+;;
+;; Code:
+
+(define %deb-build-system-modules
+  ;; Build-side modules imported by default.
+  `((lotus build deb-build-system)
+    ,@patchelf:%patchelf-build-system-modules))
+
+(define (default-patchelf)
+  "Return the default Patchelf package."
+  ;; Lazily resolve the binding to avoid a circular dependency.
+  (let ((patchelf-mod (resolve-interface '(gnu packages elf))))
+    (module-ref patchelf-mod 'patchelf)))
+
+(define* (lower name
+                #:key source inputs native-inputs outputs system target ;; host-inputs
+                (patchelf (default-patchelf))
+                #:allow-other-keys
+                #:rest arguments)
+  "Return a bag for NAME."
+  (define private-keywords
+    '(#:target #:deb #:inputs #:native-inputs)) ;#:host-inputs
+
+  (and (not target)                               ;XXX: no cross-compilation
+       (bag
+         (name name)
+         (system system)
+         (host-inputs `(,@(if source
+                              `(("source" ,source))
+                              '())
+                        ,@inputs
+                        ;; Keep the standard inputs of 'gnu-build-system'.
+                        ,@(gnu:standard-packages)))
+         (build-inputs `(("binutils" ,binutils)
+                         ("patchelf" ,patchelf)
+                         ,@native-inputs))
+         ;; (build-inputs `(,@(if source
+         ;;                       `(("source" ,source))
+         ;;                       '())
+         ;;                 ,@native-inputs
+         ;;                 ,@(if (and target implicit-cross-inputs?)
+         ;;                       (standard-cross-packages target 'host)
+         ;;                       '())
+         ;;                 ,@(if implicit-inputs?
+         ;;                       (standard-packages)
+         ;;                       '())))
+         (outputs outputs)
+         (build deb-build)
+         (arguments (strip-keyword-arguments private-keywords arguments)))))
+
+(define* (deb-build store name inputs
+                         #:key source ;; host-inputs
+                         (tests? #f)
+                         (parallel-tests? #t)
+                         (test-command ''("make" "check"))
+                         (phases '(@ (lotus build deb-build-system)
+                                     %standard-phases))
+                         (outputs '("out"))
+                         (output-libs ''("/lib"))
+                         (search-paths '())
+                         (system (%current-system))
+                         (guile #f)
+                         (imported-modules %deb-build-system-modules)
+                         (modules '((lotus build deb-build-system)
+                                    (guix build utils))))
+  "Build SOURCE using DEB, and with INPUTS."
+  (define builder
+    `(begin
+       (use-modules ,@modules)
+       (deb-build #:name ,name
+                       #:source ,(match (assoc-ref inputs "source")
+                                   (((? derivation? source))
+                                    (derivation->output-path source))
+                                   ((source)
+                                    source)
+                                   (source
+                                    source))
+                       #:system ,system
+                       #:test-command ,test-command
+                       #:tests? ,tests?
+                       #:phases ,phases
+                       #:outputs %outputs
+                       #:output-libs ,output-libs
+                       ;; #:exclude ,exclude
+                       #:search-paths ',(map search-path-specification->sexp
+                                             search-paths)
+                       ;; #:host-inputs ',inputs
+                       #:inputs %build-inputs)))
+
+  (define guile-for-build
+    (match guile
+      ((? package?)
+       (package-derivation store guile system #:graft? #f))
+      (#f                                         ; the default
+       (let* ((distro (resolve-interface '(gnu packages commencement)))
+              (guile  (module-ref distro 'guile-final)))
+         (package-derivation store guile system #:graft? #f)))))
+
+  (build-expression->derivation store name builder
+                                #:inputs inputs
+                                #:system system
+                                #:modules imported-modules
+                                #:outputs outputs
+                                #:guile-for-build guile-for-build))
+
+(define deb-build-system
+  (build-system
+    (name 'deb)
+    (description "The build system for Deb packages")
+    (lower lower)))
+
+;;; deb.scm ends here
---
+++ lotus/build/deb-build-system.scm
@@ -0,0 +1,94 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 Federico Beffa <beffa@fbengineering.ch>
+;;; Copyright © 2016 David Thompson <davet@gnu.org>
+;;; Copyright © 2016 Alex Kost <alezost@gmail.com>
+;;; Copyright © 2018, 2019 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (lotus build deb-build-system)
+  #:use-module ((guix build gnu-build-system)       #:prefix gnu:)
+  #:use-module ((lotus build patchelf-build-system) #:prefix patchelf:)
+  #:use-module (guix build utils)
+  ;; #:use-module (gnu packages bootstrap)
+  ;; #:use-module (lotus build deb-utils)
+  #:use-module (ice-9 ftw)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-11)
+  #:use-module (srfi srfi-26)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ice-9 regex)
+  #:use-module (ice-9 match)
+  #:export (%standard-phases
+            deb-build))
+
+
+;; Commentary:
+;;
+;; Builder-side code of the build procedure for ELPA Deb packages.
+;;
+;; Code:
+
+(define gnu:unpack (assoc-ref gnu:%standard-phases 'unpack))
+
+(define* (unpack #:key source #:allow-other-keys)
+  "Unpack SOURCE into the build directory.  SOURCE may be a compressed
+archive, a directory, or an Emacs Lisp file."
+  (let ((cwd (getcwd)))
+    (mkdir "data")
+    (chdir "data")
+    (invoke "ar" "xv" source)
+    (chdir cwd)
+    (mkdir "debdata")
+    (let* ((files (find-files "data")))
+      (chdir "debdata")
+      (for-each (lambda (file)
+                  (format #t "checking ~a~%" file)
+                  (when (string-prefix? "data/data" file)
+                    (format #t "matched unpacking ~a~%" file)
+                    (gnu:unpack #:source (string-append cwd "/" file))))
+                files)
+      (chdir cwd)
+      (copy-recursively "debdata/usr" "source")
+      (chdir "source")
+      #t)))
+
+
+(define %standard-phases
+  (modify-phases patchelf:%standard-phases
+    ;; (replace 'unpack unpack)
+    ;; (add-after 'unpack 'add-source-to-load-path add-source-to-load-path)
+    (replace 'unpack unpack)
+    (delete  'bootstrap)
+    (delete  'configure)
+    ;; (replace 'build build)
+    (delete  'check)))
+    ;; (replace 'install install)
+
+    ;; (add-after 'install 'make-autoloads make-autoloads)
+    ;; (add-after 'make-autoloads 'patch-el-files patch-el-files)
+    ;; ;; The .el files are byte compiled directly in the store.
+    ;; (add-after 'patch-el-files 'build build)
+    ;; (add-after 'build 'move-doc move-doc)
+
+(define* (deb-build #:key inputs (phases %standard-phases)
+                      #:allow-other-keys #:rest args)
+  "Build the given Deb package, applying all of PHASES in order."
+  (apply patchelf:patchelf-build
+         #:inputs inputs #:phases phases
+         args))
+
+;;; deb-build-system.scm ends here
