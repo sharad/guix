975ab8408ce136fe6ea53a25721b3e4831a6b14d
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-01-02 22:17:11 +0530

    correction

--- lotus/packages/firefox.scm
+++ lotus/packages/firefox.scm
@@ -162,15 +162,15 @@
 
                                        (format #t "deb-adobe-flashplugin ~a~%" (assoc-ref inputs "deb-adobe-flashplugin"))
                                        (system* "ls" "-ld" (assoc-ref inputs "deb-adobe-flashplugin"))
-                                       (system* "ls" "-l" (assoc-ref inputs "deb-adobe-flashplugin"))
+                                       (system* "ls" "-l" (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin/libflashplayer.so"))
+                                       (system* "ls" "-l" (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin/libpepflashplayer.so"))
 
                                        ;; (symlink "../../../lib/libflashplayer.so"    (string-append firefox-bin "/browser/plugins/libflashplayer.so"))
                                        ;; (symlink "../../../lib/libpepflashplayer.so" (string-append firefox-bin "/browser/plugins/libpepflashplayer.so"))
                                        ;; (copy-file (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin/libflashplayer.so")    (string-append firefox-bin "/browser/plugins/libflashplayer.so"))
                                        ;; (copy-file (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin/libpepflashplayer.so") (string-append firefox-bin "/browser/plugins/libpepflashplayer.so"))
 
-                                       (symlink (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin/libflashplayer.so")    (string-append firefox-bin "/browser/plugins/libflashplayer.so"))
-                                       (symlink (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin/libpepflashplayer.so") (string-append firefox-bin "/browser/plugins/libpepflashplayer.so"))
+                                       (symlink (string-append (assoc-ref inputs "deb-adobe-flashplugin") "/lib/adobe-flashplugin") (string-append firefox-bin "/browser/plugins"))
 
                                        #t)))
                                  ;; (delete 'strip)
