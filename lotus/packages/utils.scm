102bfb3cf46a12728397e9c70f33d8d0c52c8184
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-01-04 00:17:57 +0530

    correction

--- lotus/packages/utils.scm
+++ lotus/packages/utils.scm
@@ -358,7 +358,7 @@
                 "090j5lly5r6jzbnvlc3mhay6dsrd9sfrkjcgqaibm4nz8lp0f9cn"))))
     (build-system gnu:gnu-build-system)
     (inputs    `(("gconf" ,gconf)))
-    (arguments `(#:configure-flags (list "--without-gconf")))
+    (arguments `(#:configure-flags '("--without-gconf")))
     (synopsis "GNU Gnash is the GNU Flash movie player")
     (description "GNU Gnash
 
