f49c6f3aae49b44491617a2efb5925c98d1c64aa
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-01-30 13:55:42 +0530

    correction

--- lotus/packages/python-xyz.scm
+++ lotus/packages/python-xyz.scm
@@ -15,40 +15,27 @@
   (package (inherit python-colorama)
            (version "0.4.3")
            (source
-            (origin
-              (method url-fetch)
-              (uri (pypi-uri "colorama" version))
-              (sha256
-               (base32
-                "189n8hpijy14jfan4ha9f5n06mnl33cxz7ay92wjqgkr639s0vg9"))))))
+            (origin (method url-fetch)
+                    (uri (pypi-uri "colorama" version))
+                    (sha256 (base32 "189n8hpijy14jfan4ha9f5n06mnl33cxz7ay92wjqgkr639s0vg9"))))))
 
-;; https://files.pythonhosted.org/packages/source/c/camelot-py/camelot-py-0.7.3.tar.gz
-;; https://files.pythonhosted.org/packages/source/c/camelot-py/camelot_py-0.7.3-py3-none-any.whl
-;; https://files.pythonhosted.org/packages/source/c/camelot-py/camelot_py-0.7.3.whl
 ;; https://files.pythonhosted.org/packages/source/P/PyYAML/PyYAML-5.2.tar.gz
 (define-public python-pyyaml-52
   (package (inherit python-pyyaml)
            (name "python-pyyaml")
            (version "5.2")
-           (source
-            (origin
-              (method url-fetch)
-              (uri (pypi-uri "PyYAML" version))
-              (sha256
-               (base32
-                "0v1lwxbn0x2s6lbj7iqbn4gpy6rxf17jqvpcqb1jjbaq5k58xvn0"))))))
+           (source (origin (method url-fetch)
+                           (uri (pypi-uri "PyYAML" version))
+                           (sha256 (base32 "0v1lwxbn0x2s6lbj7iqbn4gpy6rxf17jqvpcqb1jjbaq5k58xvn0"))))))
 
 (define-public python-exifread
   (package
     (name "python-exifread")
     (version "2.1.2")
     (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "ExifRead" version))
-       (sha256
-        (base32
-         "1b90jf6m9vxh9nanhpyvqdq7hmfx5iggw1l8kq10jrs6xgr49qkr"))))
+     (origin (method url-fetch)
+             (uri (pypi-uri "ExifRead" version))
+             (sha256 (base32 "1b90jf6m9vxh9nanhpyvqdq7hmfx5iggw1l8kq10jrs6xgr49qkr"))))
     (build-system python-build-system)
     (home-page "https://github.com/ianare/exif-py")
     (synopsis
@@ -61,13 +48,9 @@
   (package
     (name "python-organize-tool")
     (version "1.7.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "organize-tool" version))
-       (sha256
-        (base32
-         "15yyh3ycb1f7q5ig7gq5ppwaf1wpx5dr906w80rfzlkh0z8byvh3"))))
+    (source (origin (method url-fetch)
+                    (uri (pypi-uri "organize-tool" version))
+                    (sha256 (base32 "15yyh3ycb1f7q5ig7gq5ppwaf1wpx5dr906w80rfzlkh0z8byvh3"))))
     (build-system python-build-system)
     (arguments
      `(#:phases
@@ -166,6 +149,9 @@
     (description "PDF parser and analyzer")
     (license license:expat)))
 
+;; https://files.pythonhosted.org/packages/source/c/camelot-py/camelot-py-0.7.3.tar.gz
+;; https://files.pythonhosted.org/packages/source/c/camelot-py/camelot_py-0.7.3-py3-none-any.whl
+;; https://files.pythonhosted.org/packages/source/c/camelot-py/camelot_py-0.7.3.whl
 ;; (define-public python-camelot-py
 ;;   (package
 ;;     (name "python-camelot-py")
