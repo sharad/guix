00b9030e5e147168ac9b1f71e0004c6b62eb2591
Sharad Pratap <110339+sharad@users.noreply.github.com>
2020-04-23 00:23:14 +0530

    correction

--- lotus/packages/ocaml.scm
+++ lotus/packages/ocaml.scm
@@ -1,5 +1,5 @@
 
-(define-module (more packages ocaml)
+(define-module ( packages ocaml)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
@@ -7,30 +7,32 @@
   #:use-module (guix build-system dune)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system ocaml)
+  #:use-module (gnu packages ocaml)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages))
 
 
-(package
-  (name "ocaml-ocp-indent")
-  (version "1.8.1")
-  (source
+(define-public ocaml-ocp-indent
+  (package
+   (name "ocaml-ocp-indent")
+   (version "1.8.1")
+   (source
     (origin
-      (method url-fetch)
-      (uri "https://github.com/OCamlPro/ocp-indent/archive/1.8.1.tar.gz")
-      (sha256
-        (base32
-          "0rqcvwzpwwzwqvrzg1l37jjiglrk2ijgjg1c7vy3xvc8lk9zyysw"))))
-  (build-system dune-build-system)
-  (propagated-inputs
+     (method url-fetch)
+     (uri "https://github.com/OCamlPro/ocp-indent/archive/1.8.1.tar.gz")
+     (sha256
+      (base32
+       "0rqcvwzpwwzwqvrzg1l37jjiglrk2ijgjg1c7vy3xvc8lk9zyysw"))))
+   (build-system dune-build-system)
+   (propagated-inputs
     `(("ocaml-dune" ,ocaml-dune)
       ("ocaml-cmdliner" ,ocaml-cmdliner)
       ("ocaml-base-bytes" ,ocaml-base-bytes)))
-  (home-page
+   (home-page
     "http://www.typerex.org/ocp-indent.html")
-  (synopsis
+   (synopsis
     "A simple tool to indent OCaml programs")
-  (description
+   (description
     "Ocp-indent is based on an approximate, tolerant OCaml parser and a simple stack
 machine ; this is much faster and more reliable than using regexps. Presets and
 configuration options available, with the possibility to set them project-wide.
@@ -41,4 +43,8 @@ Includes:
 - Bindings for popular editors
 - A library that can be directly used by editor writers, or just for\n  fault-tolerant/approximate parsing.
 ")
-  (license #f))
+   (license #f)))
+
+
+
+
