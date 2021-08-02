0003d44f4b5af420303ad6cb881ac778b8897bf5
Sharad Pratap <110339+sharad@users.noreply.github.com>
2021-08-02 18:54:54 +0530

    corretion

--- lotus/build-system/rpm.scm
+++ lotus/build-system/rpm.scm
@@ -142,11 +142,11 @@
               (guile  (module-ref distro 'guile-final)))
          (package-derivation store guile system #:graft? #f)))))
 
-  (build-expression->derivation store name builder
-                                #:inputs inputs
-                                #:system system
-                                #:modules imported-modules
-                                #:outputs outputs
+  (build-expression->derivation store name        builder
+                                #:inputs          inputs
+                                #:system          system
+                                #:modules         imported-modules
+                                #:outputs         outputs
                                 #:guile-for-build guile-for-build))
 
 (define rpm-build-system
