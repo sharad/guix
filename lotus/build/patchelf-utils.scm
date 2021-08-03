bcc47438563e6f86d9be4c909e51209fce2bd493
Sharad Pratap <110339+sharad@users.noreply.github.com>
2021-08-03 20:47:46 +0530

    corretion

--- lotus/build/patchelf-utils.scm
+++ lotus/build/patchelf-utils.scm
@@ -68,9 +68,7 @@
   (if (or (elf-file? file)
           (elf-pie-file? file)
           (elf-aslr-file? file))
-      (with-exception-handler (lambda (var) #f)
-        (lambda _ (invoke "sh" "-c" (format #f "readelf -x .interp ~a 2>&1 | grep 'Hex dump of section'" file)))
-        #:unwind? #f)
+      (zero? (apply system* "sh" "-c" (format #f "readelf -x .interp ~a 2>&1 | grep 'Hex dump of section'" file)))
       #f))
 
 (define (regular-file? file)
