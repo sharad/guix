4897eb9a290eb6ee4f12186c5f1d734815d9552d
Sharad Pratap <110339+sharad@users.noreply.github.com>
2024-02-17 23:40:27 +0530

    correction

--- lotus/build-system/patchelf.scm
+++ lotus/build-system/patchelf.scm
@@ -54,8 +54,7 @@
   ;; Build-side modules imported by default.
   `((lotus build patchelf-build-system)
     (lotus build patchelf-utils)
-    ;; (gnu packages bootstrap)
-    ;; (guix licenses)
+    (guix utils)
     ,@%gnu-build-system-modules))
 
 (define %default-modules
