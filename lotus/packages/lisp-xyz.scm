3d9eac85c6b802278d7a182e0cb17602693b5528
Sharad Pratap <110339+sharad@users.noreply.github.com>
2024-10-17 22:36:39 +0530

    correction

--- lotus/packages/lisp-xyz.scm
+++ lotus/packages/lisp-xyz.scm
@@ -13,39 +13,39 @@
 
 
 
-(define-public sbcl-clx
-  (let ((commit "52f457f0ba278e51dc0cbb4cab418049e6b32d9c")
-        (revision "2"))
-    (package
-      (name "sbcl-clx")
-      (version (git-version "0.7.6" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri
-          (git-reference
-           (url "https://github.com/sharplispers/clx")
-           (commit commit)))
-         (sha256
-          (base32 "16l0badm7dxwi7x5ynk1scrbrilnxi1nzz79h1v15xi6b41pf65w"))
-         (file-name (git-file-name "cl-clx" version))))
-      (build-system asdf-build-system/sbcl)
-      (native-inputs
-       (list sbcl-fiasco xorg-server-for-tests))
-      (arguments
-       (list #:phases
-             #~(modify-phases %standard-phases
-                 (add-before 'check 'prepare-test-environment
-                   (lambda _
-                     (system "Xvfb :1 &")
-                     (setenv "DISPLAY" ":1"))))))
-      (home-page "https://www.cliki.net/portable-clx")
-      (synopsis "X11 client library for Common Lisp")
-      (description "CLX is an X11 client library for Common Lisp.  The code was
-originally taken from a CMUCL distribution, was modified somewhat in order to
-make it compile and run under SBCL, then a selection of patches were added
-from other CLXes around the net.")
-      (license license:x11))))
+;; (define-public sbcl-clx
+;;   (let ((commit "52f457f0ba278e51dc0cbb4cab418049e6b32d9c")
+;;         (revision "2"))
+;;     (package
+;;       (name "sbcl-clx")
+;;       (version (git-version "0.7.6" revision commit))
+;;       (source
+;;        (origin
+;;          (method git-fetch)
+;;          (uri
+;;           (git-reference
+;;            (url "https://github.com/sharplispers/clx")
+;;            (commit commit)))
+;;          (sha256
+;;           (base32 "16l0badm7dxwi7x5ynk1scrbrilnxi1nzz79h1v15xi6b41pf65w"))
+;;          (file-name (git-file-name "cl-clx" version))))
+;;       (build-system asdf-build-system/sbcl)
+;;       (native-inputs
+;;        (list sbcl-fiasco xorg-server-for-tests))
+;;       (arguments
+;;        (list #:phases
+;;              #~(modify-phases %standard-phases
+;;                  (add-before 'check 'prepare-test-environment
+;;                    (lambda _
+;;                      (system "Xvfb :1 &")
+;;                      (setenv "DISPLAY" ":1"))))))
+;;       (home-page "https://www.cliki.net/portable-clx")
+;;       (synopsis "X11 client library for Common Lisp")
+;;       (description "CLX is an X11 client library for Common Lisp.  The code was
+;; originally taken from a CMUCL distribution, was modified somewhat in order to
+;; make it compile and run under SBCL, then a selection of patches were added
+;; from other CLXes around the net.")
+;;       (license license:x11))))
 
 
 
