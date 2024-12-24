8538e6cb5999249aaa4c8ac9a203a9ee9d312e97
Sharad Pratap <110339+sharad@users.noreply.github.com>
2024-12-24 22:35:39 +0530

    WIP from emacs: /srv/volumes/disk/vdss4b83r/house/home/s/paradise/Projects/Data/misc/ghq/github.com/sharad/guix/lotus/packages/utils.scm

--- lotus/packages/utils.scm
+++ lotus/packages/utils.scm
@@ -858,3 +858,8 @@ helps you return back to a previous state of development.")
                                         (delete-file-recursively "emacs")
                                         (delete-file-recursively "sublime"))))))
    (synopsis "help track git Work In Progress branches. vim plugin.")))
+
+
+
+vim-git-wip
+
